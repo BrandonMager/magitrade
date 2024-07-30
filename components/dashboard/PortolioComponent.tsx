bf93e006000fec45f2db40f339ba0aa15df9c877
BrandonMager <sotogarciabrandon@gmail.com>
2024-07-29 20:29:20 -0700

    Data added

--- components/dashboard/PortolioComponent.tsx
+++ components/dashboard/PortolioComponent.tsx
@@ -1,9 +1,11 @@
-import { View } from "react-native";
-import { useEffect } from "react";
+import { Text, View, ViewComponent } from "react-native";
+import { useEffect, useState } from "react";
 
+type ViewOption = "1D" | "1W"
 export default function PortfolioComponent(){
-    
+    const [view, setView] = useState<ViewOption>("1D")
+
     return <View>
-        
+        <Text> Your Porfolio</Text>
     </View>
 }
---
+++ tests/getPortolio.ts
@@ -0,0 +1,71 @@
+const exampleUser = {
+    name: "Brandon",
+    balanceHistory: [{
+        balance: 100.00,
+        timestamp: {
+            day: 12,
+            month: 3,
+            year: 2024,
+            unix: 1720827000
+        },
+        
+        
+    },
+    {
+        balance: 121.21,
+        timestamp: {
+            day: 13,
+            month: 3,
+            year: 2024,
+            unix: 1720913400
+        },
+        
+    }, {
+        balance: 112.09,
+        timestamp: {
+            day: 14,
+            month: 3,
+            year: 2024,
+            unix: 1720999800
+        },
+        },
+
+        {
+            balance: 112.09,
+            timestamp: {
+                day: 15,
+                month: 3,
+                year: 2024,
+                unix:1721086200
+            },
+        },
+        {
+            balance: 112.09,
+            timestamp: {
+                day: 16,
+                month: 3,
+                year: 2024,
+                unix: 1721172600
+            },
+        },
+        {
+            balance: 112.09,
+            timestamp: {
+                day: 17,
+                month: 3,
+                year: 2024,
+                unix: 1721259000
+            },
+        },
+        {
+            balance: 112.09,
+            timestamp: {
+                day: 18,
+                month: 3,
+                year: 2024,
+                unix: 1721345400
+            },
+        }
+    
+    ]
+}
